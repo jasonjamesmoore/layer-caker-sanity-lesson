a82b82409ba1de1d75de4f745b406914f1b3e6fd
jasonjamesmoore <111584948+jasonjamesmoore@users.noreply.github.com>
2026-01-05 13:03:26 -0500

    playing catch up with the tutorial

--- src/app/(frontend)/posts/[slug]/page.tsx
+++ src/app/(frontend)/posts/[slug]/page.tsx
@@ -2,10 +2,10 @@ import { notFound } from "next/navigation";
 import Image from "next/image";
 import Link from "next/link";
 import { PortableText } from "next-sanity";
-import { components } from "@/sanity/portableTextComponents";
 import { sanityFetch } from "@/sanity/lib/live";
 import { POST_QUERY } from "@/sanity/lib/queries";
 import { urlFor } from "@/sanity/lib/image";
+import { components } from "@/sanity/portableTextComponents";
 
 export default async function Page({
   params,
@@ -40,7 +40,7 @@ export default async function Page({
       <h1 className="text-4xl font-bold text-balance">{post?.title}</h1>
       {post?.body ? (
         <div className="prose">
-          <PortableText value={post.body} components={components}/>
+          <PortableText value={post.body} components={components} />
         </div>
       ) : null}
       <hr />
