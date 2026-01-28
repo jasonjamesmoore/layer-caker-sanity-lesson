89570954ca4975b8711153f9a5a4014c7d25ff2c
jasonjamesmoore <111584948+jasonjamesmoore@users.noreply.github.com>
2026-01-28 13:03:38 -0500

    feat: update Sanity queries and add live preview support

--- src/sanity/lib/queries.ts
+++ src/sanity/lib/queries.ts
@@ -45,5 +45,34 @@ export const POST_QUERY =
   author->{
     name,
     image
+  },
+  relatedPosts[]{
+    _key, // required for drag and drop
+    ...@->{_id, title, slug} // get fields from the referenced post
   }
 }`)
+
+export const PAGE_QUERY =
+  defineQuery(`*[_type == "page" && slug.current == $slug][0]{
+  ...,
+  content[]{
+    ...,
+    _type == "faqs" => {
+      ...,
+      faqs[]->
+    }
+  }
+}`);
+
+export const HOME_PAGE_QUERY = defineQuery(`*[_id == "siteSettings"][0]{
+    homePage->{
+      ...,
+      content[]{
+        ...,
+        _type == "faqs" => {
+          ...,
+          faqs[]->
+        }
+      }      
+    }
+  }`);
